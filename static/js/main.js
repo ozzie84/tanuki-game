a0ff7473d3176d9a118348567af394a267ae1a8d
ozzie84 <terehov84@live.com>
2018-10-10 21:59:17 +0300

    game1

--- static/js/main.js
+++ static/js/main.js
@@ -3,10 +3,6 @@
 (function ($) {
   $(document).ready(function () {
     svg4everybody({});
-  }); // Nav pages global
-
-  $('.menu_click').on('click', function () {
-    $('.menu_wrap').toggleClass('menu_wrap_active');
-  }); // Page scroll to id
-  // $("a[rel='m_PageScroll2id']").mPageScroll2id();
+  });
+  var swiper = new Swiper('.swiper-container');
 })(jQuery);
