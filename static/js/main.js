5956957c5654b26e5581274fec38921a718b7275
ozzie84 <terehov84@live.com>
2018-10-18 00:48:59 +0300

    lilithcocaine3

--- static/js/main.js
+++ static/js/main.js
@@ -5,4 +5,10 @@
     svg4everybody({});
   });
   var swiper = new Swiper('.swiper-container');
+  $(document).on('click', '.active_popup', function () {
+    $('.popup_wrap').fadeIn(300);
+  });
+  $(document).on('click', '.popup_bg, .popup_close', function () {
+    $('.popup_wrap').fadeOut(300);
+  });
 })(jQuery);
